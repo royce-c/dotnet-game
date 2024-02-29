3e78de5c5248fbe81024ca5f75a72071afde7713
r <coviello.royce@gmail.com>
2024-02-29 00:07:27 -0800

    fixed vite config

--- ChatApp.Frontend/vite.config.ts
+++ ChatApp.Frontend/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
