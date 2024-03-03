c7fbb0a3b48344ac68353478b3fc818a3e81aff8
r <coviello.royce@gmail.com>
2024-03-02 22:15:05 -0800

    added transparency fade styling

--- ChatApp.Frontend/src/components/ui/board.tsx
+++ ChatApp.Frontend/src/components/ui/board.tsx
@@ -7,38 +7,30 @@ interface BoardProps {
 
 const Board: React.FC<BoardProps> = ({ tiles, onClick }) => {
   return (
-    <div className="p-4">
-      <table>
-        <tbody>
-          {tiles.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {row.map((value, columnIndex) => (
-                <td
-                  key={columnIndex}
-                  onClick={() => onClick(rowIndex, columnIndex)}
-                  style={{
-                    cursor: "pointer",
-                    width: 50,
-                    height: 50,
-                    border: "1px solid black",
-                    textAlign: "center",
-                    backgroundColor:
-                      value === 0
-                        ? "black"
-                        : value === 1
-                        ? "white"
-                        : "transparent",
-                    color: value === 0 ? "white" : "black",
-                  }}
-                >
-                  {/* {value === 0 ? "X" : value === 1 ? "O" : ""} */}
-                </td>
-              ))}
-            </tr>
+<div className="p-4">
+  <table style={{ borderCollapse: "collapse" }}>
+    <tbody>
+      {tiles.map((row, rowIndex) => (
+        <tr key={rowIndex}>
+          {row.map((value, columnIndex) => (
+                        <td
+              key={columnIndex}
+              onClick={() => onClick(rowIndex, columnIndex)}
+              className="border-fade cursor-pointer w-10 h-10 text-center"
+              style={{
+                backgroundColor: value === 0 ? "black" : value === 1 ? "white" : "transparent",
+                color: value === 0 ? "white" : "black",
+              }}
+            >
+              {/* {value === 0 ? "X" : value === 1 ? "O" : ""} */}
+            </td>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
   );
 };
 
