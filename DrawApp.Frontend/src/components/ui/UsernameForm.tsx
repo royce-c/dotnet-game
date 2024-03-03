9790aaa0ef5d52859d6a8387167bdb327c8d9a25
r <coviello.royce@gmail.com>
2024-03-03 02:29:48 -0800

    changes in frontend source

--- DrawApp.Frontend/src/components/ui/UsernameForm.tsx
+++ DrawApp.Frontend/src/components/ui/UsernameForm.tsx
@@ -23,14 +23,14 @@ const UsernameForm: React.FC<UsernameFormProps> = ({ onSubmit }) => {
         type="text"
         value={username}
         onChange={handleUsernameChange}
-        placeholder="Enter username"
+        placeholder="Write Your Name"
         className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-md"
       />
       <button
         type="submit"
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 shadow-md"
       >
-        Set Username
+        Sign Your Name
       </button>
     </form>
   );
