73ddbc938c8b4bfd604d54142e07731c9cb90527
r <coviello.royce@gmail.com>
2024-03-02 18:08:25 -0800

    tiles are now black or white

--- ChatApp.Frontend/src/components/ui/board.tsx
+++ ChatApp.Frontend/src/components/ui/board.tsx
@@ -14,16 +14,18 @@ const Board: React.FC<BoardProps> = ({ tiles, onClick }) => {
               {row.map((value, columnIndex) => (
                 <td
                   key={columnIndex}
-                  onClick={() => onClick(rowIndex, columnIndex)} // Pass rowIndex and columnIndex
+                  onClick={() => onClick(rowIndex, columnIndex)}
                   style={{
                     cursor: "pointer",
                     width: 50,
                     height: 50,
                     border: "1px solid black",
                     textAlign: "center",
+                    backgroundColor: value === 0 ? "black" : value === 1 ? "white" : "transparent",
+                    color: value === 0 ? "white" : "black",
                   }}
                 >
-                  {value === 0 ? "X" : value === 1 ? "O" : ""}
+                  {/* {value === 0 ? "X" : value === 1 ? "O" : ""} */}
                 </td>
               ))}
             </tr>
